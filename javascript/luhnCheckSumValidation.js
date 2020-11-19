b3f8ab3e8f466105543e7b42c59f812406410b24
Brandon Tripp <brandontripp@MacBook-Pro.lan>
2020-11-19 13:34:27 -0700

    luhn Checksum solution for even length inputs of any length. Missing the ability to dynamically check both even and odd length inputs

--- javascript/luhnCheckSumValidation.js
+++ javascript/luhnCheckSumValidation.js
@@ -27,13 +27,43 @@ function doubleDigitValue(digit){
    return sum;
 }
 
+// Simple Checksum for any number of even length 
 
-// Simple Checksum fixed length
-let checksum = 0;
+
+// rl.question('Enter a six-digit number: ', function(num){
+//    let checksum = 0;
+//    // Splits String from input into array
+//    let numString = num.split('');
+//    // Iterate through the array of individual digits
+//    for(let digitPosition = 0; digitPosition < num.length; digitPosition++){
+//       // Transform string of number in number type
+//       let digitInt = parseInt(numString[digitPosition]);
+//       // Check if digit needs to be doubled // Only for even numbered inputs
+//       if ((digitPosition + 1) % 2 === 0) {
+//          checksum += digitInt; 
+//       } else {
+//          checksum += doubleDigitValue(digitInt);
+//       }
+//    }
+//    // Output the checksum and result of validity
+//    process.stdout.write('Checksum is ' + checksum + '\n');
+//    if (checksum % 10 === 0){
+//       process.stdout.write('Checksum is divisible by 10. Valid. \n');
+//    } else {
+//       process.stdout.write('Checksum is not divisible by 10. Invalid. \n');
+//    }
+// })
+
+
 
 rl.question('Enter a six-digit number: ', function(num){
-   let numString = num.toString(7).split('');
-   for(let digitPosition = 0; digitPosition < 6; digitPosition++){
+   console.log('input type', typeof(num));
+   console.log("num length", num.length);
+   let checksum = 0;
+   // Splits String from input into array
+   let numString = num.split('');
+   console.log('num split string', numString);
+   for(let digitPosition = 0; digitPosition < num.length; digitPosition++){
       let digitInt = parseInt(numString[digitPosition]);
       if ((digitPosition + 1) % 2 === 0) {
          checksum += digitInt; 
@@ -45,9 +75,6 @@ rl.question('Enter a six-digit number: ', function(num){
    if (checksum % 10 === 0){
       process.stdout.write('Checksum is divisible by 10. Valid. \n');
    } else {
-      process.stdout.write('Checksum is divisible by 10. Invalid. \n');
+      process.stdout.write('Checksum is not divisible by 10. Invalid. \n');
    }
 })
-
-
-
